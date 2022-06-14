ae9332b279ec338bde750acb73d2cb132af8bab8
Suyeon Park <s2026@e-mirim.hs.kr>
2022-06-14 10:24:26 +0900

    #17 윈도 폼 - 메서드 활용

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
-    public partial class Form1 : Form
+    public partial class btnStart : Form
     {
-        public Form1()
+        private int elapsedTime = 0;
+        public btnStart()
         {
             InitializeComponent();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.elapsedTime++;
+            this.lblStatus.Text = this.elapsedTime + "초 경과";
+            this.tbStatus.Text = this.elapsedTime + "초 경과";
+            this.progressBar1.Value = this.elapsedTime;
+            if (this.progressBar1.Maximum == this.progressBar1.Value)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                btnStart.Text = "시작~";
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
